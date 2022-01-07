1dbcf4fb9080a5027e9dd3407e8c497e16011aa0
Thies C. Arntzen <thieso@gmail.com>
2022-01-07 19:38:47 +0100

    import

---
+++ ProductNameXcode/ProductNameXcodeApp.swift
@@ -0,0 +1,18 @@
+//
+//  ProductNameXcodeApp.swift
+//  ProductNameXcode
+//
+//  Created by Thies C. Arntzen on 07.01.22.
+//
+
+import SwiftUI
+
+@main
+struct ProductNameXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello, world!")
+                .padding()
+        }
+    }
+}
